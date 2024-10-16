621740fd3a4c23cc1573474285990c689ad5cb0b
rak <82490156+rakDev21@users.noreply.github.com>
2024-10-16 08:45:14 +0800

    Initial commit

---
+++ LunchRoller/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("來決定今天的午餐吧......\n");
+
+var lunch = new List<string>();
+
+var streamReader = new StreamReader(".\\LunchMenu.txt");
+var content = streamReader.ReadToEnd();
+if (string.IsNullOrEmpty(content))
+{
+    Console.WriteLine("\n選單中無內容可用。");
+}
+
+var lunchMenu = content.Split(',');
+foreach (var item in lunchMenu)
+{
+    lunch.Add(item);
+}
+
+var target = new Random().Next(0, lunch.Count - 1);
+
+Console.WriteLine($"\n今日午餐：{lunch[target]}\n");
+
+Console.ReadKey();
